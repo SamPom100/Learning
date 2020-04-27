099df45c090ae33da2191e34c7ea569f7a2b7aa6
SamPom100 <sampom100@gmail.com>
2020-04-27 10:41:30 -0500

    added to swift

--- swift_sandbox/MyPlayground.playground/Contents.swift
+++ swift_sandbox/MyPlayground.playground/Contents.swift
@@ -1,5 +1,12 @@
 import UIKit
 
+//string
 var str = "Hello, playground"
+
+//constant
+let cont = "This won't change!"
+
+//multiple on a line needs a semicolon
+let cat = "Cat"; print(cat)
 
 print(str)
