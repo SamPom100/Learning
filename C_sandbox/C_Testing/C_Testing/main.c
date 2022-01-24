6c79f8584e818e5f75dab4ac7e14fd8e3d64e389
Sam Pomerantz <sampomer@bu.edu>
2022-01-24 13:08:51 -0500

    new learn

---
+++ C_sandbox/C_Testing/C_Testing/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  C_Testing
+//
+//  Created by Sam Pomerantz on 1/20/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+
+
+
