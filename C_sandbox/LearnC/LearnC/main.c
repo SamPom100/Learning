3e071ee72ff64498003722959982bf092ddb5e65
Sam Pomerantz <sam@sampomerantz.me>
2022-04-12 14:03:21 -0400

    learning c

---
+++ C_sandbox/LearnC/LearnC/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  LearnC
+//
+//  Created by Sam Pomerantz on 4/12/22.
+//
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!\n\n");
+   return 0;
+}
