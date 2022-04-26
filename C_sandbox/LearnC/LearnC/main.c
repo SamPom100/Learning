c460536295941339c4699fa7a719ab4df0f3542c
Sam Pomerantz <sam@sampomerantz.me>
2022-04-26 14:36:21 -0400

    working with C

--- C_sandbox/LearnC/LearnC/main.c
+++ C_sandbox/LearnC/LearnC/main.c
@@ -5,28 +5,49 @@
 //  Created by Sam Pomerantz on 4/12/22.
 //
 
+
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Person{
+    char name[10];
+    int age;
+    char* location;
+};
+
+struct Person normalValues[] = {
+    [0] = {.name = "hi", .age = 25, .location = "local"}
+};
 
 
-long XARRAY[1024];
-
-long sumit(void){
-    long i = 0;
-    long sum = 0;
+int main(){
+    struct Person p1;
+    p1.age = 15;
     
-    for(i=0; i<10; i++){
-        sum += XARRAY[i];
-    }
-    return sum;
-}
-
-
-int mainTwo() {
-    long test = sumit();
-    XARRAY[0] = 1;
-    printf("%ld", test);
-    printf("%ld", XARRAY[0]);
-    printf("\n\nHello, World!\n\n");
+    struct Person p2 = {.name = "test", .age = 20, .location = "Boston"};
+    printf("%d\n", p2.age);
+    
+    struct Person *p3 = &normalValues[0];
+    printf("%d\n", p3->age);
+    
+    struct Person p4 = normalValues[0];
+    printf("%d\n", p4.age);
+    
+    p3->age = 30;
+    
+    printf("%d\n", p4.age);
+    
+    struct Person p5 = normalValues[0];
+    printf("%d\n", p5.age);
+    
+    
+    
+    int size = 3;
+    struct Person *peopleArray = (struct Person*)malloc(size*sizeof(struct Person));
+    peopleArray[0] = p1;
+    peopleArray[1] = p2;
+    peopleArray[2] = *p3;
+    
+    
     return 0;
 }
-
